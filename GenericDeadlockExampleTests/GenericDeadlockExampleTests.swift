c3f7821ba74dd24cd90c1399221f85228ef34eff
Jeff Hui <jeff@jeffhui.net>
2015-02-24 00:43:07 -0800

    Added bug.

--- GenericDeadlockExampleTests/GenericDeadlockExampleTests.swift
+++ GenericDeadlockExampleTests/GenericDeadlockExampleTests.swift
@@ -1,36 +1,29 @@
-//
-//  GenericDeadlockExampleTests.swift
-//  GenericDeadlockExampleTests
-//
-//  Created by Jeff Hui on 2/24/15.
-//  Copyright (c) 2015 Jeff Hui. All rights reserved.
-//
-
 import UIKit
 import XCTest
 
+public struct Node<T: Equatable> {
+    let value: T
+    let children : [Node<T>]
+
+    public init(value: T) {
+        self.value = value
+        self.children = []
+    }
+}
+
+public class Fn<T> {
+    let fn : (random: Int, size: UInt) -> T
+    public init(_ generator: (random: Int, size: UInt) -> T) {
+        fn = generator
+    }
+}
+
+public func nodeFactory() -> Fn<Node<Int>> {
+    return Fn({ rnd, s in Node(value: 1) })
+}
+
 class GenericDeadlockExampleTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
     func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        nodeFactory()
     }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
